bdfe1e365abf277c23b58194b0310ff034ebe7b0
ConJoined <39203143+ConJoined@users.noreply.github.com>
2018-05-29 06:33:21 -0500

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -7,8 +7,8 @@ exports.run = (Discord, client, message, args) => {
         .setColor([135, 21, 153])
         .setThumbnail("https://cdn.discordapp.com/avatars/450288720151707658/81a8e401f8e4f58292de9de35f85bf18.png?size=256")
 
-        .addField("Build", "1005", true)
-        .addField("Build Changes", "Extension of Build 1003/1004:\n- Added kill switch for some commands in DMs\n-Added this pane\n- New commands\n-Eval commands have combined in to one\n- New update command for users with eval command perm\n- Restart and Shutdown command for if the bot is going haywire; no more Heroku need", true)
+        .addField("Build", "1006", true)
+        .addField("Build Changes", "- Multiple prefixes (ppu!, pro pigeon unstable, ping)", true)
         .addField("Version", "Professional Unstable", true)
         .addField("Server Count", client.guilds.size.toString(), true)
         .addField("Servers Inside", client.guilds.map(g => g.name).join("\n"), true)
